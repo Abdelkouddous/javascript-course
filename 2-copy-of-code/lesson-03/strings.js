483e1f71307e3f360016730a2709274afe2dede6
abdelkouddousHamel <abdelkouddous.hamel@gmail.com>
2024-07-06 18:29:39 +0100

    Advancement in course

--- 2-copy-of-code/lesson-03/strings.js
+++ 2-copy-of-code/lesson-03/strings.js
@@ -1,4 +1,4 @@
-'hello'
+/** 'hello'
 alert('hello');
 
 'some' + 'text'
@@ -14,14 +14,23 @@ typeof 'hello'
 
 'Items (' + (1 + 1) + '): $' + (2095 + 799) / 100
 alert('Items (' + (1 + 1) + '): $' + (2095 + 799) / 100);
+//second way to create strings ""
 
 "hello"
 "I'm learning JavaScript"
-
+//escape characters
 'I\'m learning JavaScript'
 alert('some\ntext');
-
+//backtick `
 `hello`
 `Items (${1 + 1}): $${(2095 + 799) / 100}`
 `some
-text`
+text`*/
+let items = document.getElementById('items').innerText;
+let shipping = document.getElementById('shipping').innerText;
+const tax = 0.1;
+
+function sum(){
+  let sum = items+shipping;
+  return sum=document.getElementById('total').textContent;
+}
