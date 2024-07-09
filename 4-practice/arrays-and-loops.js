045057bdd68816162fce20b1d7e44a64f2eff208
Abdelkouddous <58855794+Abdelkouddous@users.noreply.github.com>
2024-07-09 18:04:06 +0100

    done part 11

--- 4-practice/arrays-and-loops.js
+++ 4-practice/arrays-and-loops.js
@@ -8,45 +8,63 @@
 // // typeof[ x, y]
 // // length
 // // isArray
+
+//const { default: BaseComponent } = require("bootstrap/js/dist/base-component");
+
+// const { render } = require("react-dom");
+
 // creating empty array that stocks the todoelement
 const toDoList = [
   {
-    name: "study",
-    date: "2024-07-09",
+    name: "Default",
+    date: "12-12-2012",
   },
 ];
-let counter = 0;
 
 //function to remove element
 function removeTodo() {
   // toDoList.pop();
-  // console.log(toDoList);
-  for (let i in toDoList) {
-    toDoList.splice(i, 1);
-    console.log(toDoList);
-  }
+
+  for (let i = 0; i < toDoList.length; i++) {}
 }
 
 //function to add the element
 function addTodo() {
-  const inputElement = document.querySelector(".js-name-input");
-  const inputDate = document.querySelector(".js-date-input");
-  const name = inputElement.value;
-  const date = inputDate.value;
+  let counter = 0;
+  const getName = document.querySelector(".js-name-input");
+  const getDate = document.querySelector(".js-date-input");
+  name = getName.value;
+  date = getDate.value;
+  counter += 1;
+  console.log(name + " $" + date);
   toDoList.push({
     name,
     date,
   });
-  counter += 1;
-  console.log(name + " $" + date);
+  renderToDoList();
+}
+
+function renderToDoList() {
+  let todoListHTML = "";
+  for (let i = 1; i < toDoList.length; i++) {
+    todo = toDoList[i];
+    const { name, date } = todo;
+    const html = `<input type = 'checkbox'/>   
+    ${name} ${date} 
+    <button type="button" onclick="    
+    toDoList.splice(${i}, 1);
+    console.log(toDoList);
+    renderToDoList();" class = "delete-btn">Delete
+    </button> <br>
+   `;
+    todoListHTML += html;
+  }
   //we add paragraph output
   //
-  document.querySelector(
-    ".paragraph-output"
-  ).innerHTML += `<input type = 'checkbox'/>   ${counter}  ${name}  ${date} <button type="button" onclick="removeTodo()
-      " class = "delete-btn">Delete</button> <br>`;
-  // localStorage.setItem(".js-name-input", name);;
+  document.querySelector(".paragraph-output").innerHTML = todoListHTML;
+  // localStorage.setItem(".js-name-input", name);
 }
+
 //array function of exerice 11i
 function array(x = [], y) {
   for (let i in x) {
