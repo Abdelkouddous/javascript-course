50626fa6502143495ca172fdfdfa82003627b465
Abdelkouddous <58855794+Abdelkouddous@users.noreply.github.com>
2024-07-09 16:48:21 +0100

    same

--- 4-practice/arrays-and-loops.js
+++ 4-practice/arrays-and-loops.js
@@ -9,25 +9,48 @@
 // // length
 // // isArray
 // creating empty array that stocks the todoelement
-const toDoList = [];
+const toDoList = [
+  {
+    name: "study",
+    date: "2024-07-09",
+  },
+];
 let counter = 0;
-var date = Date();
+
+//function to remove element
+function removeTodo() {
+  // toDoList.pop();
+  // console.log(toDoList);
+  for (let i in toDoList) {
+    toDoList.splice(i, 1);
+    console.log(toDoList);
+  }
+}
 
 //function to add the element
 function addTodo() {
   const inputElement = document.querySelector(".js-name-input");
+  const inputDate = document.querySelector(".js-date-input");
   const name = inputElement.value;
-  toDoList.push(name);
+  const date = inputDate.value;
+  toDoList.push({
+    name,
+    date,
+  });
   counter += 1;
-  console.log(name + " $" + toDoList);
-  document.querySelector(".paragraph-output").innerHTML +=
-    "<input type = 'checkbox'/> " +
-    " " +
-    counter +
-    " " +
-    name +
-    "<br>" +
-    date +
-    "<br>";
-  // localStorage.setItem(".js-name-input", name);
+  console.log(name + " $" + date);
+  //we add paragraph output
+  //
+  document.querySelector(
+    ".paragraph-output"
+  ).innerHTML += `<input type = 'checkbox'/>   ${counter}  ${name}  ${date} <button type="button" onclick="removeTodo()
+      " class = "delete-btn">Delete</button> <br>`;
+  // localStorage.setItem(".js-name-input", name);;
+}
+//array function of exerice 11i
+function array(x = [], y) {
+  for (let i in x) {
+    x[i] = x[i] * y;
+  }
+  console.log(x);
 }
